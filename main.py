f9116d02b56ccd96ef554c711df95b6530ffb057
Artur-Settarov0228 <settarovartur2@gmail.com>
2026-02-01 19:26:20 +0500

    read file

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-with open("pi.text") as file:
-    pi = file.read()
+# with open("pi.text") as file:
+#     pi = file.read()
 
-pi = pi.rstrip()
-pi = pi.replace("\n", "")
+# pi = pi.rstrip()
+# pi = pi.replace("\n", "")
 
 
-print(pi)
+# print(pi)
 
+
+# filename = 'data/talabalar.text'
+# # with open(filename) as file:
+# #     for line in file:
+# #         print(line)
+
+
+# with open(filename) as file:
+#     talabalar = file.readlines()
+
+# print(talabalar)
+
+# for talaba in talabalar:
+#     talabalar = talaba.rstrip()
+# print(talabalar)
+
+# talabalar = [talaba.rstrip() for talaba in talabalar]
+# print(talabalar)
